f4f4e3f4675553a8bf6f731dc86af8a9bb5d2606
John Bryant <john.r.bryant@gmail.com>
2023-12-05 15:49:56 +0700

    made last age group open

--- data-raw/src/synmort.R
+++ data-raw/src/synmort.R
@@ -8,9 +8,10 @@ cmd_assign(deaths = "out/deaths.rds",
 
 synmort <- inner_join(deaths, popn,
                       by = c("age", "sex", "indig", "region", "time")) %>%
-    arrange(region, indig, time, sex, age) %>%
-    mutate(popn = round(popn)) %>%
-    as.data.frame()
+  arrange(region, indig, time, sex, age) %>%
+  mutate(popn = round(popn)) %>%
+  mutate(age = ifelse(age == 100, "100+", age)) %>%
+as.data.frame()
 
 save(synmort, file = .out)
 
